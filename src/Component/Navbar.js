098d9ec40761420bc9db91e9cb1214781aa59832
Vikas85109 <vikassharma85109@gmail.com>
2022-01-15 12:22:44 +0530

    2 files modified

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,14 +4,16 @@ import {Link} from 'react-router-dom';
 export default Navbar;
 function Navbar(){
   return (
-    <nav>
+    <nav >
+      <Link to ="/">Home</Link>
+      &nbsp;&nbsp;&nbsp;
+      
+      <Link to ="/contact">Contact</Link>
+      &nbsp;&nbsp;&nbsp;
       <Link to="/about" className="link">
       About
       </Link>
-      &nbsp;&nbsp;&nbsp;
-      <Link to ="/contact">Contact</Link>
-      &nbsp;&nbsp;&nbsp;
-      <Link to ="/">Home</Link>
+      
     </nav>
   );
 }
